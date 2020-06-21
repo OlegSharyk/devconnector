6ea5f877c4b885c23d8f32edd06f2aca57f0d2e4
OlegSharyk <docshark@gmail.com>
2020-06-21 23:07:04 +0300

    get repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,15 +7,72 @@ import {
     UPDATE_PROFILE,
     CLEAR_PROFILE,
     ACCOUNT_DELETED,
+    GET_PROFILES,
+    GET_REPOS,
 } from './types';
 
 // GET current users profiles
 export const getCurrentProfile = () => async (dispatch) => {
     try {
-        const res = await axios.get('api/profile/me');
+        const res = await axios.get('/api/profile/me');
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({ type: CLEAR_PROFILE });
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status },
+        });
+    }
+};
+
+// get all profiles
+export const getProfiles = () => async (dispatch) => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status },
+        });
+    }
+};
+
+// get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status },
+        });
+    }
+};
+
+// get Github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data,
         });
     } catch (err) {
@@ -169,7 +226,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
     if (window.confirm('Are you sure? This can NOT undone!')) {
         try {
-            const res = await axios.delete('/api/profile');
+            await axios.delete('/api/profile');
 
             dispatch({
                 type: CLEAR_PROFILE,
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -44,7 +44,8 @@ const EditProfile = ({
             youtube: loading || !profile.social ? '' : profile.social.youtube,
             instagram: loading || !profile.social ? '' : profile.social.instagram,
         });
-    }, [loading]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
